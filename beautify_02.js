369f839a788cbe2fb843caddc4cefbe171844e5e
viet61193 <103628005+viet61193@users.noreply.github.com>
2024-06-18 15:31:05 +0700

    Create beautify_02.js

---
+++ beautify_02.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+            var editor = ace.edit('fromeditor');
+            editor.getSession().setMode('ace/mode/css');
+            editor.setShowPrintMargin(false);
+
+            var toEditor = ace.edit('toeditor');
+            toEditor.getSession().setMode('ace/mode/css');
+            toEditor.setShowPrintMargin(false);
+
+            $('#convert').click(function () {
+                var editorContent = editor.getValue();
+                var formattedContent = vkbeautify.css(editorContent, '');
+                toEditor.getSession().setValue(formattedContent);
+            });
+
+            $('#reset').click(function () {
+                editor.getSession().setValue('');
+                toEditor.getSession().setValue('');
+            });
+
+            $('#copy').click(function () {
+                var toEditorContent = toEditor.getValue();
+
+                var textarea = document.createElement('textarea');
+                textarea.value = toEditorContent;
+                document.body.appendChild(textarea);
+
+                textarea.select();
+                document.execCommand('copy');
+
+                document.body.removeChild(textarea);
+
+                alert('Nội dung đã được sao chép vào clipboard.');
+            });
+
+            function getCurrentYear() {
+                var d = new Date();
+                return d.getFullYear();
+            }
+
+            var el = document.getElementById('current-year');
+            if (el) {
+                el.innerHTML = getCurrentYear();
+            }
+        });
